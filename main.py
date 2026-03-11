3c8c42f77285712c6c4e0a623554a1662da88043
vivid-py <dobrostanandrij109+work@gmail.com>
2026-03-11 23:31:05 +0200

    Feat: add sidebar, add textbox to enter some text you need now

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import customtkinter as ctk 
-
 class App(ctk.CTk):
+    textboxFont=("Inter", 13)
     def __init__(self):
         super().__init__()
         self.geometry("960x540")
         self.title("Zen Write")
         self.resizable(False, False)
-
+        self.genSidebar()
+        self.genUI()
+    def genSidebar(self):
+        self.sidebar = ctk.CTkScrollableFrame(self, height=540, width=200)
+        self.sidebar._scrollbar.grid_forget()
+        self.sidebar.pack(side="left")
+    def genUI(self):
+        self.text = ctk.CTkTextbox(self,width=760, height=540, font=self.textboxFont).pack()
+    
 app = App()
 app.mainloop()
